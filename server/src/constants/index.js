ffef0974b83775608f17ea28bd1f5b3b28c19888
Carlos Espinosa <carloespinosa88@gmail.com>
2022-06-21 19:55:52 -0500

    feat: add apollo client and some other config

---
+++ server/src/constants/index.js
@@ -0,0 +1,7 @@
+const urlPhoto = 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg';
+const urlThumbnail = 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg';
+
+module.exports = {
+  urlPhoto,
+  urlThumbnail
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const {urlPhoto, urlThumbnail } = require('./constants/index');
 const typeDefs = require('./schema');
 
 const mocks = {
@@ -13,12 +14,10 @@ const mocks = {
     author: () => {
       return {
         name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
+        photo: urlPhoto
       };
     },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    thumbnail: () => urlThumbnail,
     length: () => 1210,
     modulesCount: () => 6
   })
